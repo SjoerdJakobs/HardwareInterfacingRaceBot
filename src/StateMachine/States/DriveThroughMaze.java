b08a052be06a93ad55c2eebd9747e415fdc1f389
kruimeldief <d.iemeel@miracom.nl>
2019-12-09 11:56:15 +0100

    DriveThroughMaze update. v1

--- src/StateMachine/States/DriveThroughMaze.java
+++ src/StateMachine/States/DriveThroughMaze.java
@@ -2,8 +2,10 @@ package StateMachine.States;
 
 import Modules.Engine;
 import Modules.Phototransistor;
+import Modules.Ultrasonic;
 import StateMachine.State;
 import StateMachine.StateID;
+import TI.BoeBot;
 
 public class DriveThroughMaze extends State {
 
@@ -15,11 +17,18 @@ public class DriveThroughMaze extends State {
 
     private boolean hasLine;
     private Engine engine;
+    private int direction = 0; // -1 = left, 0 = mid, 1 = right, 2 = go back!
+    private boolean currentTurning = false;
+    private int numberOfTurns = 0;
 
-    Phototransistor pin0 = new Phototransistor(0);
+    Phototransistor pin0 = new Phototransistor(0); // links
     Phototransistor pin1 = new Phototransistor(1);
     Phototransistor pin2 = new Phototransistor(2);
-    Phototransistor pin3 = new Phototransistor(3);
+    Phototransistor pin3 = new Phototransistor(3); // rechts
+    Ultrasonic uLeft = new Ultrasonic(6); //links
+    Ultrasonic uMid = new Ultrasonic(7); //midden
+    Ultrasonic uRight = new Ultrasonic(8); //rechts
+
 
     @Override
     protected void enter()
@@ -41,34 +50,65 @@ public class DriveThroughMaze extends State {
     protected void logic()
     {
         super.logic();
-        if(pin1.detectLine()||pin2.detectLine())
-        {
-            // write code here
+
+        if (!currentTurning) {
+
+            if (pin0.detectLine() && pin1.detectLine() && pin2.detectLine() && pin3.detectLine()) {
+                if (!uLeft.detectWall()) {
+                    engine.Steer(true, 2);
+                    currentTurning = true;
+                }
+                else if (!uMid.detectWall()) {
+                    engine.Steer(true, 0);
+                    currentTurning = false;
+                }
+                else if (!uRight.detectWall()) {
+                    engine.Steer(false, 2);
+                    currentTurning = true;
+                }
+                else {
+                    engine.Steer(false, 2);
+                    currentTurning = true;
+                }
+                BoeBot.wait(1000); // To prevent the BoeBot from detecting the line when starting to turn
+            }
+
+            // Make sure to stay on the line while not turning
+            if (pin1.detectLine() || pin2.detectLine()) {
+                engine.Steer(true, 0);
+            }
+            else if (pin2.detectLine()) {
+                engine.Steer(false, 0.6);
+            }
+            else if (pin1.detectLine()) {
+                engine.Steer(true, 0.6);
+            }
+            else if (pin0.detectLine() || pin1.detectLine()) {
+                engine.Steer(true, 0.75);
+            }
+            else if (pin2.detectLine() || pin3.detectLine()) {
+                engine.Steer(false, 0.75);
+            }
+            else if (pin3.detectLine()) {
+                engine.Steer(false, 0.99);
+            }
+            else if (pin0.detectLine()) {
+                engine.Steer(true, 0.99);
+            }
+
         }
-        else if(pin2.detectLine())
-        {
-            // write code here
+
+        // When turning, continue detecting a line after which the turning will stop
+        if (currentTurning) {
+
+            if (pin1.detectLine() || pin2.detectLine()) {
+                currentTurning = false;
+                engine.Steer(false, 0);
+            }
+
         }
-        else if(pin1.detectLine())
-        {
-            // write code here
-        }
-        else if(pin0.detectLine()||pin1.detectLine())
-        {
-            // write code here
-        }
-        else if(pin2.detectLine()||pin3.detectLine())
-        {
-            // write code here
-        }
-        else if(pin3.detectLine())
-        {
-            // write code here
-        }
-        else if(pin0.detectLine())
-        {
-            // write code here
-        }
+
+
         engine.drive();
     }
 
